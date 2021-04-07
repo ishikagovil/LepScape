e7345ac449133c494f5e6e4d51cc742e5f508fbb
Arunima Dey <arunimad@udel.edu>
2021-04-07 15:16:56 -0400

    fix: Edit Conditions and Leps

--- src/main/java/Lep.java
+++ src/main/java/Lep.java
@@ -5,18 +5,25 @@ import java.util.ArrayList;
 //a description of the lep, and all the native plants that it can feed on to survive
 public class Lep {
 	
-	public String speciesName;
-	public String genusName;
-	public String commonName;
-	public String description;
-	public ArrayList<PlantSpecies> thrivesIn; //all the plants species the lep feeds on
+	private String speciesName;
+	private String genusName;
+	private String commonName;
+	private String description;
+	private ArrayList<PlantSpecies> thrivesIn; //all the plants species the lep feeds on
 	
-	public Lep(String speciesName, String genusName, String commonName, String description, ArrayList<PlantSpecies> thrivesIn) {}
+	public Lep(String speciesName, String genusName, String commonName) {
+		this.speciesName = speciesName;
+		this.genusName = genusName;
+		this.commonName = commonName;
+	}
+	
+	public void setDescription(String des) {this.description = des;}
+	public void setThrivesIn(ArrayList<PlantSpecies> thrives) {this.thrivesIn = thrives;}
 	
 	public String getSpeciesName() {return this.speciesName;}
 	public String getGenusName() {return this.genusName;}
 	public String getCommonName() {return this.commonName;}
 	public String getDescription() {return this.description;}
-	public ArrayList<PlantSpecies> getThrivesIn(){return this.getThrivesIn();}
+	public ArrayList<PlantSpecies> getThrivesIn(){return this.thrivesIn;}
 	
 }
