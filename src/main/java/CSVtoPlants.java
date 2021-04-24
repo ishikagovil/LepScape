01e4c32aec08bfaa0b036825f561d2d9d7d407be
Dea Harjianto <dea@udel.edu>
2021-04-24 11:56:35 -0400

    fix: loading images into ManageViews

--- src/main/java/CSVtoPlants.java
+++ src/main/java/CSVtoPlants.java
@@ -61,7 +61,9 @@ public class CSVtoPlants {
 		Map<String, ImageView> plantImg = new HashMap<>();
         
         String genusName, speciesName;
-        String imgSrc;
+        String imgSrc, filePath;
+        
+        System.out.println("beginning images");
 		
 		try {
 		    BufferedReader reader = new BufferedReader(new FileReader(fileName));
@@ -71,8 +73,11 @@ public class CSVtoPlants {
                 String[] parts = line.split(","); // separate the line by comma
                 genusName = parts[0];
                 speciesName = parts[1];
-                imgSrc = genusName + "-" + speciesName + ".png";            // file name ex: Justicia-americana.png
-                Image img = new Image(CSVtoPlants.class.getResourceAsStream("../resources/plantimg/" + imgSrc));    // file path to get image names; change if different file path
+                imgSrc = genusName + "-" + speciesName + ".png";		// file name ex: Justicia-americana.png
+                filePath = "plantimg/" + imgSrc;
+                System.out.println(filePath);
+                Image img = new Image(filePath);    // file path to get image names; change if different file path
+                System.out.println("fetching images");
                 ImageView imv = new ImageView();
                 imv.setImage(img);
                 imv.setFitWidth(100);                       // resizing to be 100 x 100px; can change value
