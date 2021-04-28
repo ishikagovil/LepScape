7044a5d9c8e8fd839e272fecb91f05e0b0a7ed74
Jinay Jain <jinaybjain@gmail.com>
2021-04-28 09:05:02 -0400

    test: Add tests for Condition

--- src/test/java/ConditionsTest.java
+++ src/test/java/ConditionsTest.java
@@ -1,38 +1,71 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.*;
+
+import javafx.scene.paint.Color;
 
 class ConditionsTest {
-	SoilType soil;
-	Conditions n = new Conditions(soil,10,2);
+	Conditions c;
 	
-	@Test
-	void testGetSoil() {
-//		fail("Not yet implemented");
-		assertEquals(n.getSoilType(),5);
+	@BeforeEach
+	void createCondition() {
+		c = new Conditions(SoilType.CLAY, MoistureType.MOIST, LightType.INTENSE);
 	}
 	
 	@Test
-	void testGetMoisture(){
-		assertEquals(10,n.getMoistureLevel());
+	void testGetSoilType() {
+		assertEquals(c.getSoilType(), SoilType.CLAY);
+	}
+	@Test
+	void testSetSoilType() {
+		c.setSoilType(SoilType.DIRT);
+		assertEquals(c.getSoilType(), SoilType.DIRT);
+	}
+	@Test
+	void testGetMoistureType() {
+		assertEquals(c.getMoistureType(), MoistureType.MOIST);
+	}
+	@Test
+	void testSetMoistureType() {
+		c.setMoistureType(MoistureType.ANY);
+		assertEquals(c.getMoistureType(), MoistureType.ANY);
+	}
+	@Test
+	void testGetSunlightType() {
+		assertEquals(c.getSunlightType(), LightType.INTENSE);
+	}
+	@Test
+	void testSetSunlightType() {
+		c.setSunlightType(LightType.ANY);
+		assertEquals(c.getSunlightType(), LightType.ANY);
 	}
 	
 	@Test
-	void testGetSun() {
-		assertEquals(2,n.getSunlight());
+	void testGetAndSetX() {
+		c.setX(5.0);
+		assertEquals(c.getX(), 5.0);
+	}
+	@Test
+	void testGetAndSetY() {
+		c.setY(12.0);
+		assertEquals(c.getY(), 12.0);
+	}
+	@Test
+	void testToColor() {
+		Color col = c.toColor();
+		assertEquals(col.getRed(), 0.3, 1e-4);
+		assertEquals(col.getGreen(), 0.6, 1e-4);
+		assertEquals(col.getBlue(), 0.9, 1e-4);
 	}
 	
 	@Test
-	void test4() {
-		ArrayList<float[]> x = new ArrayList<>();
-		float[] n1 = {1,2};
-		float[] n2 = {0,1};
-		float[] n3 = {7,5};
-		x.add(n1);
-		x.add(n2);
-		x.add(n3);
-		//n.setSectionsOutlines(x);
-		//assertEquals(x,n.getSections());
+	void testFromColor() {
+		Color col = new Color(0.0, 0.0, 0.9, 1.0);
+		Conditions newCond = Conditions.fromColor(col);
+		assertEquals(newCond.getSoilType(), SoilType.ANY);
+		assertEquals(newCond.getSunlightType(), LightType.INTENSE);
+		assertEquals(newCond.getMoistureType(), MoistureType.ANY);
 	}
 
 }
